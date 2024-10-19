505b0340f6c74e0693f854312c81412cc4f91fcc
Matbubble <matiasjimenezalcocer@gmail.com>
2024-10-19 17:46:57 -0400

    depas

---
+++ Obi/Departamentales.cpp/B.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <bitset>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+#define dbg(x) cerr<<#x<<": "<<x<<"\n";
+
+/*  
+POKER FACE ._.
+*/
+
+void solve(){
+    long long n; cin>>n;
+    long long discriminador=(8ll*n + 1ll);
+    long long xd=sqrt(discriminador);
+    long long pro=(xd-1ll)/2ll;
+    cout<<pro<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
