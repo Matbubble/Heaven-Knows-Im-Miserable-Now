8684fc89c143410a45c1f6a7bba25b2bf40ea14a
Matbubble <matiasjimenezalcocer@gmail.com>
2024-12-06 15:43:46 -0400

    Create D.cpp

---
+++ Codeforces/Educational172.cpp/D.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <bitset>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+#define dbg(x) cerr<<#x<<": "<<x<<"\n";
+using T=pair<long long, long long>;
+/*  
+Time waits for nobody
+*/
+
+
+
+
+void solve(){
+    long long n; cin>>n;
+    vector<T>vp(n);
+    for(T& x:vp) cin>>x.first>>x.second;
+    sort(vp.begin(), vp.end(), [&](T a, T b) -> bool {
+        return (a.first == b.first) ? a.second > b.second : a.first < b.first;
+    });
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin>>t;
+    while(t--) solve();
+    return 0;
+}
